a38f9ef095f2be97e09e334468180d1a8c8cbcbc
pieperm <michaelepieper@gmail.com>
2018-01-04 12:53:44 -0600

    Basic create event page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,33 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule} from "@angular/forms";
 import { EventListComponent } from './event-list/event-list.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { CreateEventComponent } from './create-event/create-event.component';
+import {
+  MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatInputModule, MatNativeDateModule,
+  MatStepperModule, MatTableModule, MatTooltipModule
+} from "@angular/material";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventListComponent
+    EventListComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTooltipModule,
+    MatTableModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
