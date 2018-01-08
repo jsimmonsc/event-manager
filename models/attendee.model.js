dca6dbd05a57f2497ede4c073ec21e1cc3bc53d7
greg-bahr <greg.bahr123@gmail.com>
2018-01-08 10:13:58 -0600

    added models for attendee and events

---
+++ models/attendee.model.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var attendeeSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  student_number: Number,
+  grade_level: Number,
+  guest: String,
+  guestId: Number,
+  timestamp: String
+});
+
+module.exports = mongoose.model('Attendee', attendeeSchema);
