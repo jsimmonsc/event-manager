c34bb1ae3cb9ca11918d205f99806663f6b72ca5
Greg Bahr <greg.bahr123@gmail.com>
2018-01-19 08:53:22 -0600

    added cors

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 require('./routes/student.routes')(app);
 require('./routes/event.routes')(app);
