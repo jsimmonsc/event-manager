c50cd532b5b09ae53ff51c5c60a0b30d293032fa
greg-bahr <greg.bahr123@gmail.com>
2018-01-19 12:45:05 -0600

    fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -487,11 +487,11 @@ module.exports = {
     "setImmediate": false
   },
   "devServer": {
-    "historyApiFallback": true
-  headers: {
-    "Access-Control-Allow-Origin": "http://capstone.psdr3.org:3000",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-  }
+    "historyApiFallback": true,
+    headers: {
+      "Access-Control-Allow-Origin": "http://capstone.psdr3.org:3000",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   }
 };
