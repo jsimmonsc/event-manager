65d0a9e2404884d7848c824cf8a5fa38355c106c
greg-bahr <greg.bahr123@gmail.com>
2018-01-22 14:10:35 -0600

    makes sure input is numeric

--- src/app/check-in/check-in.component.ts
+++ src/app/check-in/check-in.component.ts
@@ -21,7 +21,7 @@ export class CheckInComponent {
 
   searchForAttendee(studentNumber: string): void {
     this.attendee = null;
-    if (studentNumber && studentNumber.length === 5) {
+    if (+studentNumber && studentNumber.length === 5) {
       this.eventService.getAttendeeFromEvent(this.id, +studentNumber).subscribe(att => {
         this.attendee = att;
       }, err => {
