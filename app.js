19092d18cacedc27fa34feb3dfdd51752e8ca485
Greg Bahr <greg.bahr123@gmail.com>
2018-01-23 10:43:22 -0600

    added cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var dbConfig = require('./config/db.config');
 var RestClient = require('node-rest-client').Client;
 var app = express();
 var Student = require('./models/student.model');
+var cors = require('cors');
 
 var restClient = new RestClient();
 
@@ -28,11 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 require('./routes/student.routes')(app);
 require('./routes/event.routes')(app);
