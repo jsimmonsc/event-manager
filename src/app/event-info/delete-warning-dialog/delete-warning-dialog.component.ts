1dfdfb59c748a270cd2ea6c26c079b6f58459d60
greg-bahr <greg.bahr123@gmail.com>
2018-02-06 08:59:23 -0600

    made delete dialog delete attendee if yes is clicked, close if no is clicked

--- src/app/event-info/delete-warning-dialog/delete-warning-dialog.component.ts
+++ src/app/event-info/delete-warning-dialog/delete-warning-dialog.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material";
+import {EventService} from "../../shared/services/event.service";
+import {Attendee} from "../../shared/models/attendee.model";
 
 @Component({
   selector: 'app-delete-warning-dialog',
   templateUrl: './delete-warning-dialog.component.html',
   styleUrls: ['./delete-warning-dialog.component.scss']
 })
-export class DeleteWarningDialogComponent implements OnInit {
+export class DeleteWarningDialogComponent {
 
-  constructor() { }
+  attendee: Attendee;
 
-  ngOnInit() {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              private eventService: EventService,
+              private matDialog: MatDialog) {
+
+    this.attendee = data.attendee;
+
+  }
+
+  removeAttendee(): void {
+    this.eventService.deleteAttendee(this.data.eventID, this.attendee).subscribe(value => {
+      this.matDialog.closeAll();
+    }, err => {
+      console.log(err);
+      // TODO: Error dialog
+    });
   }
 
 }
