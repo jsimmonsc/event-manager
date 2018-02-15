c1fc75c363e59000a7881d4932f000dd722107d0
greg-bahr <greg.bahr123@gmail.com>
2018-02-15 08:05:02 -0600

    updated .gitignore

--- config/auth.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  clientID: "678183473143-j06sed957ehs94eqkvtrrft3okreor4u.apps.googleusercontent.com",
-  clientSecret: "QbzuGKZnIriZ9ce2-nQ7gCvL",
-  callbackURL: "http://localhost:3000/auth/google/callback"
-}
