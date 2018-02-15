66a0d09ecce5a69013a7a339f1168fcd54f8befd
greg-bahr <greg.bahr123@gmail.com>
2018-02-15 12:44:53 -0600

    added nullcheck to userprofile

--- src/app/routing/guards/can-activate-sell.guard.ts
+++ src/app/routing/guards/can-activate-sell.guard.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import {AuthService} from "../../shared/services/auth/auth.service";
 
 @Injectable()
 export class CanActivateSellGuard implements CanActivate {
 
-  constructor(private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.userProfile) { this.router.navigate(['/login']); }
     const allowed = ['admin', 'super', 'seller'];
 
     return allowed.includes(this.authService.userProfile.role);
