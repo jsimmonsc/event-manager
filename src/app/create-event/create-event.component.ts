50ce439a50ef642c9c27d2e3f6e910baf404580d
pieperm <michaelepieper@gmail.com>
2018-02-16 13:00:48 -0600

    Fixed alignments and navbar position, restricted input, added more styles

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, Validators, FormBuilder, FormControl, FormArray} from '@angular/forms';
+import {FormGroup, Validators, FormBuilder, FormControl, FormArray, FormGroupDirective, NgForm} from '@angular/forms';
 import {SlidingDialogService, SlidingDialogType} from "../shared/services/sliding-dialog.service";
 import {EventService} from "../shared/services/event.service";
 import {Event} from "../shared/models/event.model";
+import {ErrorStateMatcher} from "@angular/material";
 
 @Component({
   selector: 'app-create-event',
@@ -12,6 +13,13 @@ import {Event} from "../shared/models/event.model";
 export class CreateEventComponent implements OnInit {
 
   formGroup: FormGroup;
+
+  eventNameCtrl = new FormControl('', [Validators.required]);
+  eventDescriptionCtrl = new FormControl('');
+  dateCtrl = new FormControl('', [Validators.required]);
+  costCtrl = new FormControl('', [Validators.required]);
+
+  matcher = new CustomErrorStateMatcher();
 
   constructor(private formBuilder: FormBuilder, private slidingDialog: SlidingDialogService, private eventService: EventService) {
     this.createForm();
@@ -23,36 +31,62 @@ export class CreateEventComponent implements OnInit {
 
   createEvent() {
 
-    const nameString = this.formGroup.get("eventNameCtrl").value;
-    const descriptionString = this.formGroup.get("eventDescriptionCtrl").value;
-    const dateString = this.formGroup.get("dateCtrl").value;
-    const costString = this.formGroup.get("costCtrl").value;
+    this.eventService.createEvent({
+      _id: null,
+      name: this.getFormValue("eventNameCtrl"),
+      description: this.getFormValue("eventDescriptionCtrl"),
+      date: new Date(this.getFormValue("dateCtrl")),
+      sales: 0,
+      attendees: null
+    }).subscribe((event: Event) => {
+      this.slidingDialog.displayNotification("Successfully created event", SlidingDialogType.SUCCESS);
+      console.log(JSON.stringify(event));
+    }, (err) => {
+      this.slidingDialog.displayNotification("Error creating event", SlidingDialogType.ERROR);
+      console.log(err);
+    });
+  }
 
-    this.slidingDialog.displayNotification("heyo you got a problem", SlidingDialogType.ERROR);
+  inputIsInvalid(): boolean {
 
-    // this.eventService.createEvent({
-    //   _id: null,
-    //   name: nameString,
-    //   description: descriptionString,
-    //   date: new Date(dateString),
-    //   sales: 0,
-    //   attendees: null
-    // }).subscribe((event: Event) => {
-    //   this.slidingDialog.displayNotification("Successfully created event", SlidingDialogType.SUCCESS);
-    //   console.log(JSON.stringify(event));
-    // }, (err) => {
-    //   this.slidingDialog.displayNotification("Error creating event", SlidingDialogType.ERROR);
-    //   console.log(err);
-    // });
+    return this.getFormValue("eventNameCtrl") === "" ||
+      this.getFormValue("dateCtrl") === "" ||
+      this.getFormValue("costCtrl") === "";
+
+  }
+
+  getFormValue(formControl: string): string {
+    return this.formGroup.get(formControl).value;
   }
 
   createForm() {
     this.formGroup = this.formBuilder.group({
-        eventNameCtrl: ['', Validators.required],
-        eventDescriptionCtrl: ['', Validators.required],
-        dateCtrl: ['', Validators.required],
-        costCtrl: ['', Validators.required, Validators.pattern("^[0-9]*$")]
+        eventNameCtrl: this.eventNameCtrl,
+        eventDescriptionCtrl: this.eventDescriptionCtrl,
+        dateCtrl: this.dateCtrl,
+        costCtrl: this.costCtrl
     });
   }
 
+  restrictInputToNumbers(event: any) {
+    const pattern = /[0-9\.]/;
+    const inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+
+  }
+
+
 }
+
+export class CustomErrorStateMatcher implements ErrorStateMatcher {
+
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+
+}
+
