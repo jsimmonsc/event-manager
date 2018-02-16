9fc8589059764e90bf93553725090f108e2f226d
greg-bahr <greg.bahr123@gmail.com>
2018-02-16 10:14:57 -0600

    jwt validation on routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -3,12 +3,12 @@ const validateJWT = require('../auth/jwt-validator');
 
 module.exports = (app) => {
     app.post('/events', validateJWT, controller.create);
-    app.get('/events', controller.findAll);
-    app.get('/events/id/:id', controller.findOne);
-    app.put('/events/id/:id', controller.updateOne);
-    app.put('/events/id/:id/:ticketnum', controller.updateAttendee);
-    app.post('/events/id/:id', controller.createAttendee);
-    app.delete('/events/id/:id/:ticketnum', controller.deleteAttendee);
-    app.delete('/events/id/:id', controller.deleteEvent);
-    app.get('/events/id/:id/:studentid', controller.findOneAttendee);
+    app.get('/events', validateJWT, controller.findAll);
+    app.get('/events/id/:id', validateJWT, controller.findOne);
+    app.put('/events/id/:id', validateJWT, controller.updateOne);
+    app.put('/events/id/:id/:ticketnum', validateJWT, controller.updateAttendee);
+    app.post('/events/id/:id', validateJWT, controller.createAttendee);
+    app.delete('/events/id/:id/:ticketnum', validateJWT, controller.deleteAttendee);
+    app.delete('/events/id/:id', validateJWT, controller.deleteEvent);
+    app.get('/events/id/:id/:studentid', validateJWT, controller.findOneAttendee);
 }
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,7 +1,8 @@
-var controller = require('../controllers/student.controller');
+const controller = require('../controllers/student.controller');
+const validateJWT = require('../auth/jwt-validator');
 
 module.exports = (app) => {
 
-    app.get('/students/:id', controller.findOne);
+    app.get('/students/:id', validateJWT, controller.findOne);
 }
 
