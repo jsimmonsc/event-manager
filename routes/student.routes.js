d10a491d95d64d9087d0045f3cf9432947259cc3
greg-bahr <greg.bahr123@gmail.com>
2018-02-16 13:36:29 -0600

    added guard to student searching

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,8 +1,8 @@
 const controller = require('../controllers/student.controller');
 const validateJWT = require('../auth/jwt-validator');
+const guards = require('../auth/auth-guards');
 
 module.exports = (app) => {
-
-    app.get('/students/:id', validateJWT, controller.findOne);
+    app.get('/students/:id', validateJWT, guards.seller, controller.findOne);
 }
 
