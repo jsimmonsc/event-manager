67cdc995a8e0ccd12d4ae161d8b46f4edddc3578
greg-bahr <greg.bahr123@gmail.com>
2018-04-05 10:33:37 -0500

    added Dockerfile

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    finesURI: "",
-    attendanceURI: "",
-    lunchFinesURI: ""
+    finesURI: "https://powerschool.psdr3.org:8443/ords/ps/capstone/students/hs",
+    attendanceURI: "https://powerschool.psdr3.org:8443/ords/ps/capstone/notninety/hs",
+    lunchFinesURI: "http://mosaic.psdr3.org/export/negbalance.csv",
+    dbURI: "mongodb://localhost:27017/event-manager"
 }
