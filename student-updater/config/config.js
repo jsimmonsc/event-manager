c398b4b5faaebad6f9fc7d6098b097ff8f532bc3
greg-bahr <greg.bahr123@gmail.com>
2018-05-04 13:52:12 -0500

    fixed docker compose

--- student-updater/config/config.js
+++ student-updater/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     finesURI: "https://powerschool.psdr3.org:8443/ords/ps/capstone/students/hs",
     attendanceURI: "https://powerschool.psdr3.org:8443/ords/ps/capstone/notninety/hs",
     lunchFinesURI: "http://mosaic.psdr3.org/export/negbalance.csv",
-    dbURI: "mongodb://localhost:27017/event-manager"
+    dbURI: "mongodb://mongo/event-manager"
 }
